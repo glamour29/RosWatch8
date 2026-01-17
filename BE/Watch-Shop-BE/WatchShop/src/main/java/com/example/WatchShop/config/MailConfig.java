2eb76c0246cad7ada503fd7292edfe26953a9e84
An <duylt0022@ut.edu.vn>
2026-01-18 04:55:19 +0700

    Fix MailConfig to properly configure JavaMailSender from properties

--- BE/Watch-Shop-BE/WatchShop/src/main/java/com/example/WatchShop/config/MailConfig.java
+++ BE/Watch-Shop-BE/WatchShop/src/main/java/com/example/WatchShop/config/MailConfig.java
@@ -1,6 +1,6 @@
 package com.example.WatchShop.config;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.mail.javamail.JavaMailSender;
@@ -11,10 +11,32 @@ import java.util.Properties;
 @Configuration
 public class MailConfig {
 
+    @Value("${spring.mail.host}")
+    private String host;
+
+    @Value("${spring.mail.port}")
+    private int port;
+
+    @Value("${spring.mail.username}")
+    private String username;
+
+    @Value("${spring.mail.password}")
+    private String password;
+
     @Bean
-    @ConditionalOnProperty(name = "spring.mail.host")
     public JavaMailSender javaMailSender() {
         JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "false");
+
         return mailSender;
     }
 }
